bea275edcd8b1de19b2259857b348bbd0fcce7c5
wouterwln <wouternuijten@gmail.com>
2025-01-30 11:12:44 +0000

    Deploying to gh-pages from @ wouterwln/wouterwln.github.io@2b72cde4972b70087a81dddffda63a4f857de5d4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,18 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-futsal-team-simulator",
+        },{id: "projects-futsal-team-simulator",
           title: 'Futsal Team Simulator',
           description: "Simulating player attendance for optimal team size",
           section: "Projects",handler: () => {
